7c91e9ab5d67dfcd8f2dfa04cfcba528032bc244
Анна <anakurilec4@gmail.com>
2024-11-21 17:04:50 +0300

    Update settings.py

--- project/settings.py
+++ project/settings.py
@@ -11,11 +11,11 @@ USE_DATABASE = env.bool("USE_DATABASE", default=True)
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.postgresql_psycopg2" if USE_DATABASE else "",
-        "HOST": env("APP_HOST") if USE_DATABASE else "",
-        "PORT": env("APP_PORT") if USE_DATABASE else 0,
-        "NAME": env("APP_NAME") if USE_DATABASE else "",
-        "USER": env("APP_USER") if USE_DATABASE else "",
-        "PASSWORD": env("APP_PASSWORD") if USE_DATABASE else "",
+        "HOST": env("DB_HOST") if USE_DATABASE else "",
+        "PORT": env("DB_PORT") if USE_DATABASE else 0,
+        "NAME": env("DB_NAME") if USE_DATABASE else "",
+        "USER": env("DB_USER") if USE_DATABASE else "",
+        "PASSWORD": env("DB_PASSWORD") if USE_DATABASE else "",
     }
 }
 
